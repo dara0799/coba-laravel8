c5ca35ab8a44d96a2260bb008798a07864aad6f1
dara0799 <daraayufahira.daf@gmail.com>
2022-01-03 17:35:07 +0700

    video 5.2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -15,22 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/blog', function () {
-    // $blog_posts = "";
-        
-        return view('posts', [
-            "title" => "BLOG",
-            "posts" => Post::all()
-        ]);
-    });
+Route::get('/blog', [PostController::class, 'index']);
     
-Route::get('/blog/{slug}', function ($slug) {
-
-    return view('post', [
-        "title" => "SINGLE POST",
-        "post" => Post::find($slug)
-    ]);
-});
+Route::get('/blog/{slug}', [PostController::class, 'show']);
 
 Route::get('/about', function () {
     return view('about', [
