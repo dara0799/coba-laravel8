aa0a74cc20b31c1209b1d2e5e2fed94509490a39
dara0799 <daraayufahira.daf@gmail.com>
2022-01-04 16:24:54 +0700

    video 7.2

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     @foreach($posts as $post)
         <article class="mb-5">
-            <a href="/blog/{{ $post->id }}">
+            <a href="/blog/{{ $post->slug }}">
                 <h2>{{ $post->title }}</h2>
             </a>
             <h5>By: {{ $post->author }}</h5>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/blog', [PostController::class, 'index']);
     
-Route::get('/blog/{slug}', [PostController::class, 'show']);
+Route::get('/blog/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/about', function () {
     return view('about', [
